0f7755b69577b9ef5f614210672499ea911621c9
Alexander Feygin <feyginalex@yandex.ru>
2023-02-26 19:32:39 +0300

    дополнил скрипт datamart_ddl согласно согласно замечанию ревьера

--- datamart_ddl.sql
+++ datamart_ddl.sql
@@ -1,4 +1,5 @@
 --создаем витрину в схеме analysis
+drop table if exists analysis.dm_rfm_segments;
 create table analysis.dm_rfm_segments (
     user_id int4 NOT NULL, 
     recency smallint NOT NULL CHECK (recency >= 1 and recency <= 5),
